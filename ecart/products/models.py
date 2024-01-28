90d6516a742dc808c3b85d9efeaf212c2f459312
mohammed sahal <mhdsahal155@gmail.com>
2024-01-29 00:22:50 +0530

    data model design

---
+++ ecart/products/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# product models
+
+class Product(models.Model):
+    LIVE=1
+    DELETE=0
+    DELETE_CHOICES=((LIVE,'live'),(DELETE,'delete'))
+    title = models.CharField(max_length=200)
+    price = models.BooleanField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='media/')
+    priority = models.IntegerField(default=0)
+    delete_status = models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
