3b29f0b95dff481a993bf186c98b02fec233f5cf
mohammed sahal <mhdsahal155@gmail.com>
2024-02-10 23:18:39 +0530

    project worked till account section

--- ecart/products/models.py
+++ ecart/products/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     DELETE=0
     DELETE_CHOICES=((LIVE,'live'),(DELETE,'delete'))
     title = models.CharField(max_length=200)
-    price = models.BooleanField()
+    price = models.FloatField()
     description = models.TextField()
     image = models.ImageField(upload_to='media/')
     priority = models.IntegerField(default=0)
